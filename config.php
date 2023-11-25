601fbb3b547e003b4af9da98d5fafcd0d92ff615
Developer Douglas <dglsdeveloper@gmail.com>
2023-11-26 00:18:00 +0300

    Update: Update db name configuration

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $host = "localhost"; /* Host name */
 $user = "root"; /* User */
 $password = ""; /* Password */
-$dbname = "tutorial"; /* Database name */
+$dbname = "ajax-search"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 // Check connection
